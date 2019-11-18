0e9d70b708cdf5539a696770d6bd8021226ece85
raftutak <raftutak@gmail.com>
2019-11-18 01:16:37 +0100

    Updated categories hardcoded list

--- src/data/categories.js
+++ src/data/categories.js
@@ -82,35 +82,213 @@ export const categories = [
         name: 'Desery mleczne'
       }
     ]
+  },
+  {
+    id: 2,
+    name: 'Zupy',
+    subcategories: [
+      {
+        id: 0,
+        name: 'Zupy klasyczne'
+      },
+      {
+        id: 1,
+        name: 'Zupy owocowe'
+      },
+      {
+        id: 2,
+        name: 'Chłodniki'
+      },
+      {
+        id: 3,
+        name: 'Różne'
+      },
+      {
+        id: 4,
+        name: 'Zupy kremy'
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Dania',
+    subcategories: [
+      {
+        id: 0,
+        name: 'Dania wegetariańskie'
+      },
+      {
+        id: 1,
+        name: 'Dania śniadaniowe'
+      },
+      {
+        id: 2,
+        name: 'Dania z wołowiny i wieprzowiny'
+      },
+      {
+        id: 3,
+        name: 'Dania z mięsa mielonego'
+      },
+      {
+        id: 4,
+        name: 'Dania z ryżu'
+      },
+      {
+        id: 5,
+        name: 'Dania z kaszy'
+      },
+      {
+        id: 6,
+        name: 'Dania makaronowe'
+      },
+      {
+        id: 7,
+        name: 'Dania z jaj'
+      },
+      {
+        id: 8,
+        name: 'Zapiekanki'
+      },
+      {
+        id: 9,
+        name: 'Różne'
+      },
+      {
+        id: 10,
+        name: 'Sałatki'
+      },
+      {
+        id: 11,
+        name: 'Dania z grzybów'
+      },
+      {
+        id: 12,
+        name: 'Dania z dziczyzny'
+      },
+      {
+        id: 13,
+        name: 'Dania mączne i ziemniaczane'
+      },
+      {
+        id: 14,
+        name: 'Dania z ryb i owoców morza'
+      },
+      {
+        id: 15,
+        name: 'Dania z drobiu'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Dodatki',
+    subcategories: [
+      {
+        id: 0,
+        name: 'Dodatki z ziemniaków, kaszy i ryżu'
+      },
+      {
+        id: 1,
+        name: 'Pesto'
+      },
+      {
+        id: 2,
+        name: 'Puree'
+      },
+      {
+        id: 3,
+        name: 'Sosy na gorąco'
+      },
+      {
+        id: 4,
+        name: 'Sosy na zimno'
+      },
+      {
+        id: 5,
+        name: 'Różne'
+      },
+      {
+        id: 6,
+        name: 'Surówki'
+      },
+      {
+        id: 7,
+        name: 'Pasty'
+      },
+      {
+        id: 8,
+        name: 'Wędliny'
+      },
+      {
+        id: 9,
+        name: 'Pasztety'
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: 'Przekąski',
+    subcategories: [
+      {
+        id: 0,
+        name: 'Na zimno'
+      },
+      {
+        id: 1,
+        name: 'Na ciepło'
+      }
+    ]
+  },
+  {
+    id: 6,
+    name: 'Przetwory',
+    subcategories: [
+      {
+        id: 0,
+        name: 'Różne'
+      },
+      {
+        id: 1,
+        name: 'Dżemy'
+      },
+      {
+        id: 2,
+        name: 'Powidła'
+      },
+      {
+        id: 3,
+        name: 'Marmolady'
+      },
+      {
+        id: 4,
+        name: 'Przetwory marynowane'
+      },
+      {
+        id: 5,
+        name: 'Konfitury'
+      }
+    ]
+  },
+  {
+    id: 7,
+    name: 'Pieczywo',
+    subcategories: [
+      {
+        id: 0,
+        name: 'Pieczywo inne'
+      },
+      {
+        id: 1,
+        name: 'Pieczywo pszenne'
+      },
+      {
+        id: 2,
+        name: 'Pieczywo mieszane'
+      },
+      {
+        id: 3,
+        name: 'Pieczywo żytnie'
+      }
+    ]
   }
-  // {
-  //   id: 3,
-  //   name: 'Zupy',
-  //   subcategories: []
-  // },
-  // {
-  //   id: 4,
-  //   name: 'Dania',
-  //   subcategories: []
-  // },
-  // {
-  //   id: 5,
-  //   name: 'Dodatki',
-  //   subcategories: []
-  // },
-  // {
-  //   id: 6,
-  //   name: 'Przekąski',
-  //   subcategories: []
-  // },
-  // {
-  //   id: 7,
-  //   name: 'Przetwory',
-  //   subcategories: []
-  // },
-  // {
-  //   id: 8,
-  //   name: 'Pieczywo',
-  //   subcategories: []
-  // }
 ];
