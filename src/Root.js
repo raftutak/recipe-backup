b4b9b7c2cb8d983639138c38ae5fc4629a6199bd
raftutak <raftutak@gmail.com>
2019-12-07 22:05:15 +0100

    LoginModal and RegistrationModal creation

--- src/Root.js
+++ src/Root.js
@@ -13,6 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeView from './views/HomeView';
 import CategoriesView from './views/CategoriesView';
 import RecipeView from './views/RecipeView';
+import CalculatorView from './views/CalculatorView';
 import ContactView from './views/ContactView';
 
 // TEMPLATES
@@ -20,7 +21,9 @@ import MainTemplate from './templates/MainTemplate';
 
 class Root extends React.Component {
   state = {
-    search_input: ''
+    search_input: '',
+    showLoginModal: false,
+    showRegistrationModal: false
   };
 
   handleInputChange = event => {
@@ -46,7 +49,7 @@ class Root extends React.Component {
     const response = await axios(url);
     const search_result = await response.data.recipes;
 
-    this.setState({ search_result, search_isLoading: false });
+    this.setState({ search_isLoading: false, search_result });
   };
 
   handleSubmitSearch = async event => {
@@ -58,49 +61,44 @@ class Root extends React.Component {
     });
 
     const query = this.state.search_input;
+    let url = `https://recipe-search.projektstudencki.pl/recipe/searchRecipes/?search=${query}&count=8`;
 
-    let url = `https://recipe-search.projektstudencki.pl/recipe/searchRecipes/?search=${query}&count=10`;
-
-    if (
-      this.state.search_mainCategory !== undefined &&
-      this.state.search_mainCategory !== ''
-    ) {
+    if (this.state.search_mainCategory) {
       url = url.concat(
         `&dishMainCategoryIds=${this.state.search_mainCategory}`
       );
     }
-
-    if (
-      this.state.search_subCategory !== undefined &&
-      this.state.search_subCategory !== ''
-    ) {
+    if (this.state.search_subCategory) {
       url = url.concat(`&dishSubCategoryIds=${this.state.search_subCategory}`);
     }
 
     const response = await axios(url);
     const search_result = await response.data.recipes;
+    // console.log(search_result);
 
-    console.log(search_result);
-
-    this.setState({ search_result, search_isLoading: false });
+    this.setState({ search_isLoading: false, search_result });
   };
 
   handleShowCategory = async event => {
     event.preventDefault();
 
-    this.setState({
-      search_isLoading: true,
-      search_result: undefined
-    });
+    this.setState({ categories_isLoading: true, categories_result: undefined });
 
     const id = event.target.value;
-
     let url = `https://recipe-search.projektstudencki.pl/recipe/searchRecipes/?search=&count=20&dishMainCategoryIds=${id}`;
 
     const response = await axios(url);
-    const search_result = await response.data.recipes;
+    const categories_result = await response.data.recipes;
 
-    this.setState({ search_result, search_isLoading: false });
+    this.setState({ categories_result, categories_isLoading: false });
+  };
+
+  handleShowLoginModal = event => {
+    this.setState({ showLoginModal: !this.state.showLoginModal });
+  };
+
+  handleShowRegistrationModal = event => {
+    this.setState({ showRegistrationModal: !this.state.showRegistrationModal });
   };
 
   componentDidMount() {
@@ -112,7 +110,9 @@ class Root extends React.Component {
       ...this.state,
       handleInputChange: this.handleInputChange,
       handleSubmitSearch: this.handleSubmitSearch,
-      handleShowCategory: this.handleShowCategory
+      handleShowCategory: this.handleShowCategory,
+      handleShowLoginModal: this.handleShowLoginModal,
+      handleShowRegistrationModal: this.handleShowRegistrationModal
     };
 
     return (
@@ -123,7 +123,7 @@ class Root extends React.Component {
               <MainTemplate>
                 <Route exact path={routes.home} component={HomeView} />
                 <Route path={routes.categories} component={CategoriesView} />
-                <Route path={routes.recipe} component={RecipeView} />
+                <Route path={routes.calculatorBMI} component={CalculatorView} />
                 <Route path={routes.contact} component={ContactView} />
               </MainTemplate>
             </Switch>
--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -6,12 +6,16 @@ import GlobalStyle from '../theme/GlobalStyle';
 // COMPONENTS
 import Navigation from '../components/Navigation';
 import Header from '../components/Header';
+import LoginModal from '../components/LoginModal';
+import RegistrationModal from '../components/RegistrationModal';
 
 const MainTemplate = ({ children, location }) => (
   <>
     <GlobalStyle />
     {console.log(location.pathname)}
     <Navigation />
+    <LoginModal />
+    <RegistrationModal />
     {location.pathname !== '/categories' ? <Header /> : null}
     {children}
   </>
